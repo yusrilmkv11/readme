191c31c3475056d1b9f0be1ca26f7f1d8b287713
yusrilmukav <yusrilmukav@gmail.com>
2017-05-08 20:52:32 +0800

    mau masuk bang

---
+++ print.rb
@@ -0,0 +1,19 @@
+nama = "pentel"
+alamat = "dimana"
+
+
+
+
+# print alamat
+# print nama
+
+
+# puts alamat
+# puts nama 
+
+
+# p nama
+# p alamat
+
+mantan = ["siska", "sherly", "bella"]
+puts mantan 
---
+++ why.rb
@@ -0,0 +1,10 @@
+
+
+puts "who's there?"
+
+password = gets.chomp
+if ( password == "siapa ya" )
+    puts "please enter"
+else
+    puts "get a fuck off my face"
+end
